24ac5ae759e3e0560061033860e0231c4b4009c7
master-bry <bryngowi2@gmail.com>
2025-09-21 00:01:54 +0300

    MODEL

--- app/Models/PaymentModel.php
+++ app/Models/PaymentModel.php
@@ -8,4 +8,33 @@ class PaymentModel extends Model
     protected $table = 'payment';
     protected $primaryKey = 'id';
     protected $allowedFields = ['Name', 'RoomType', 'Bed', 'cin', 'cout', 'noofdays', 'NoofRoom', 'meal', 'roomtotal', 'bedtotal', 'mealtotal', 'finaltotal'];
+
+    public function calculateTotals($booking)
+    {
+        $rates = [
+            'Superior Room' => 100000,
+            'Deluxe Room' => 80000,
+            'Guest House' => 50000,
+            'Single Room' => 30000,
+            'Single' => 5000,
+            'Double' => 10000,
+            'None' => 0,
+            'Room only' => 0,
+            'Breakfast' => 5000,
+            'Half Board' => 10000,
+            'Full Board' => 15000,
+        ];
+
+        $roomtotal = $rates[$booking['RoomType']] * $booking['NoofRoom'] * $booking['nodays'];
+        $bedtotal = $rates[$booking['Bed']] * $booking['NoofRoom'] * $booking['nodays'];
+        $mealtotal = $rates[$booking['Meal']] * $booking['NoofRoom'] * $booking['nodays'];
+        $finaltotal = $roomtotal + $bedtotal + $mealtotal;
+
+        return [
+            'roomtotal' => $roomtotal,
+            'bedtotal' => $bedtotal,
+            'mealtotal' => $mealtotal,
+            'finaltotal' => $finaltotal,
+        ];
+    }
 }
--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -7,28 +7,25 @@ class UserModel extends Model
 {
     protected $table = 'users';
     protected $primaryKey = 'id';
-    protected $allowedFields = ['Email', 'Password', 'is_staff', 'created_at', 'updated_at'];
-    protected $useTimestamps = false;
-    
-    // Check user login (both regular users and staff)
-public function checkLogin($email, $password)
-{
-    $user = $this->where('Email', $email)->first();
-    
-    if ($user && password_verify($password, $user['Password'])) {
-        return $user;
+    protected $allowedFields = ['Email', 'Password', 'Username', 'is_staff', 'created_at', 'updated_at', 'verification_code', 'is_verified'];
+    protected $useTimestamps = true;
+    protected $createdField = 'created_at';
+    protected $updatedField = 'updated_at';
+
+    public function checkLogin($email, $password)
+    {
+        $user = $this->where('Email', $email)->first();
+        if ($user && password_verify($password, $user['Password'])) {
+            return $user;
+        }
+        return false;
     }
-    
-    return false;
-}
-    
-    // Get user by email
+
     public function getByEmail($email)
     {
         return $this->where('Email', $email)->first();
     }
-    
-    // Check if email exists
+
     public function emailExists($email)
     {
         return $this->where('Email', $email)->countAllResults() > 0;
