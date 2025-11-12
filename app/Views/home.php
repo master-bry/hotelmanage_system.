0b4c6de954f44722357792ba42ce47ac0101e7c6
master-bry <bryngowi2@gmail.com>
2025-11-12 10:05:50 +0300

    new

--- app/Views/home.php
+++ app/Views/home.php
@@ -165,7 +165,7 @@
             var modal = new bootstrap.Modal(document.getElementById('guestdetailpanel'));
             modal.show();
         }
-    
+
      
         document.querySelector('input[name="check_in"]').addEventListener('change', function() {
             const checkOut = document.querySelector('input[name="check_out"]');
